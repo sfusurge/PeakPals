cbb7b5cfb82418843feaa1813a8701109562080e
katelina7 <jocelyngau@yahoo.com>
2020-11-22 20:33:59 -0800

    implemented theme toggle

--- app/src/main/java/com/example/firebase_log_in/HomeScreen.java
+++ app/src/main/java/com/example/firebase_log_in/HomeScreen.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,20 +17,36 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeScreen extends AppCompatActivity {
+    private SharedPreferences sharedPrefs;
     private boolean isDark = false;
     private BottomNavigationView navBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sharedPrefs = getSharedPreferences("Setting", Context.MODE_PRIVATE);
+        isDark = sharedPrefs.getBoolean("Theme", true);
+
+
+        if (isDark){
+            setTheme(R.style.LightTheme);
+        }else{
+            setTheme(R.style.DarkTheme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
         navBar = findViewById(R.id.bottom_navigation);
         navBar.setOnNavigationItemSelectedListener(navListener);
 
-        //landing fragment on home page after loading
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new HomeFragment()).commit();
+        int page = sharedPrefs.getInt("Fragment", 0);
+        if (page == 0){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        } else if (page == 1){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ProfileFragment()).commit();
+        }
+
     }
 
 
@@ -43,7 +61,9 @@ public class HomeScreen extends AppCompatActivity {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
-
+                    SharedPreferences sharedPrefs = getSharedPreferences("Setting", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPrefs.edit();
+                    int page = 0;
                     //set icons back to default
 
                     Menu menu = navBar.getMenu();
@@ -55,28 +75,38 @@ public class HomeScreen extends AppCompatActivity {
                         case R.id.nav_profile:
                             selectedFragment = new ProfileFragment();
                             item.setIcon(R.drawable.ic_nav_profile_true);
+                            page = 1;
                             break;
                         case R.id.nav_home:
                             selectedFragment = new HomeFragment();
                             item.setIcon(R.drawable.ic_nav_home_true);
+                            page = 0;
                             break;
                     }
-
+                    editor.putInt("Fragment", page);
+                    editor.apply();
                     getSupportFragmentManager().beginTransaction().replace
                             (R.id.fragment_container, selectedFragment).commit();
                     return true;
                 }
             };
 
-    public void switchTheme(){
+    public void switchTheme(View view){
         if (isDark){
             Toast.makeText(this, "button working!!!", Toast.LENGTH_SHORT).show();
-//            setTheme(R.style.LightTheme);
-//            isDark = false;
+            setTheme(R.style.LightTheme);
+            isDark = false;
         } else{
-//            setTheme(R.style.DarkTheme);
-//            isDark = true;
+            setTheme(R.style.DarkTheme);
+            isDark = true;
         }
+        SharedPreferences sharedPrefs = getSharedPreferences("Setting", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putBoolean("Theme", isDark);
+        editor.apply();
+        finish();
+        startActivity(getIntent().addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+        overridePendingTransition( 0, 0);
     }
 
 
